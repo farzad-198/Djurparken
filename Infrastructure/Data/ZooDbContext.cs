49bc589feb45a535c2c973145808f6d17be386ce
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-05-30 15:20:21 +0200

    Add EF Core DbContext Class

--- Infrastructure/Data/ZooDbContext.cs
+++ Infrastructure/Data/ZooDbContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class ZooDbContext
+    public class ZooDbContext:DbContext
     {
+        public ZooDbContext(DbContextOptions<ZooDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Domain.Entities.Animal> Animals { get; set; }
+        public DbSet<Domain.Entities.Habitat> Habitats { get; set; }
+        public DbSet<Domain.Entities.Visitor> Visitors { get; set; }
+        public DbSet<Domain.Entities.Visit> Visits { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
