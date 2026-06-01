9ebb7b4c56b9cd626eb3751729087b0c3683c503
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-06-01 20:32:23 +0200

    Add Migration

--- Infrastructure/Data/ZooDbContext.cs
+++ Infrastructure/Data/ZooDbContext.cs
@@ -1,21 +1,39 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class ZooDbContext:DbContext
+    public class ZooDbContext : DbContext
     {
+        public ZooDbContext()
+        {
+        }
+
         public ZooDbContext(DbContextOptions<ZooDbContext> options) : base(options)
         {
-
         }
-        public DbSet<Domain.Entities.Animal> Animals { get; set; }
-        public DbSet<Domain.Entities.Habitat> Habitats { get; set; }
-        public DbSet<Domain.Entities.Visitor> Visitors { get; set; }
-        public DbSet<Domain.Entities.Visit> Visits { get; set; }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        public DbSet<Habitat> Habitats { get; set; }
+
+        public DbSet<Visitor> Visitors { get; set; }
+
+        public DbSet<Visit> Visits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=FARZAD\\SQLEXPRESS;Database=DjurparkenDb;Trusted_Connection=True;TrustServerCertificate=True;"
+                );
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
-
     }
 }
---
+++ Infrastructure/Migrations/20260601183036_Djurparken.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Djurparken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habitats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Climate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Vegetation = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habitats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visitors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HabitatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animals_Habitats_HabitatId",
+                        column: x => x.HabitatId,
+                        principalTable: "Habitats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VisitDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HasPaidTicket = table.Column<bool>(type: "bit", nullable: false),
+                    VisitorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Visits_Visitors_VisitorId",
+                        column: x => x.VisitorId,
+                        principalTable: "Visitors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_HabitatId",
+                table: "Animals",
+                column: "HabitatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_VisitorId",
+                table: "Visits",
+                column: "VisitorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Visits");
+
+            migrationBuilder.DropTable(
+                name: "Habitats");
+
+            migrationBuilder.DropTable(
+                name: "Visitors");
+        }
+    }
+}
